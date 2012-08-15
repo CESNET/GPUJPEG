e64c9637d85443f2b0f460383b9156c0fc11e803
Martin Pulec <martin.pulec@seznam.cz>
2012-08-15 09:12:59 +0200

    UG specific autogen.sh changes

--- autogen.sh
+++ autogen.sh
@@ -10,13 +10,20 @@ else
     LIBTOOLIZE=libtoolize
 fi
 
+if [ ! -x ../ltmain.sh ]
+then
+        cd ..
+        $LIBTOOLIZE --copy
+        cd -
+fi
+
 autoheader && \
 $LIBTOOLIZE --copy && \
 ( [ -d m4 ] || mkdir m4 ) && \
 aclocal -I m4 && \
 automake --copy --add-missing && \
 autoconf && \
-./configure "$@"
+[ -n "$DO_NOT_CONFIGURE" ] || ./configure "$@"
 
 STATUS=$?
 
