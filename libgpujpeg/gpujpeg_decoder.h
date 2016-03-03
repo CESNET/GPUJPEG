fffe99a0dc9376c0eff8d33e41d19dc837615dc4
Martin Srom <srom.martin@gmail.com>
2016-03-03 06:53:59 +0100

    Fixed cmake file to build shared library and fixed exporting of gpujpeg_decoder_get_image_info symbol.

--- libgpujpeg/gpujpeg_decoder.h
+++ libgpujpeg/gpujpeg_decoder.h
@@ -137,7 +137,7 @@ gpujpeg_decoder_init(struct gpujpeg_decoder* decoder, struct gpujpeg_parameters*
  * @param param_image
  * @return 0 if succeeds, otherwise nonzero
  */
-int
+GPUJPEG_API int
 gpujpeg_decoder_get_image_info(uint8_t* image, int image_size, struct gpujpeg_image_parameters * param_image);
 
 /**
