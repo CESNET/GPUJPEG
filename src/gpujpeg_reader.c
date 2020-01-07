f403e1c04eb361d5bd52f25d7f694a461a97a83c
Martin Pulec <martin.pulec@cesnet.cz>
2020-01-07 13:48:07 +0100

    Correctly check retval of gpujpeg_decoder_init

--- src/gpujpeg_reader.c
+++ src/gpujpeg_reader.c
@@ -736,8 +736,9 @@ gpujpeg_reader_read_sos(struct gpujpeg_decoder* decoder, uint8_t** image, uint8_
     // We must init decoder before data is loaded into it
     if ( decoder->reader->comp_count == 0 ) {
         // Init decoder
-        GPUJPEG_CHECK_EX(gpujpeg_decoder_init(decoder, &decoder->reader->param, &decoder->reader->param_image),
-                "Cannot initialize decoder!", return -1);
+        if ( gpujpeg_decoder_init(decoder, &decoder->reader->param, &decoder->reader->param_image) != 0 ) {
+            return -1;
+        }
     }
 
     // Check maximum component count
