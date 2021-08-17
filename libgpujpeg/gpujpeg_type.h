b620be27ef70374255e88b979a84a656f4562b95
Martin Pulec <martin.pulec@cesnet.cz>
2021-08-17 13:24:36 +0200

    Doubled huffman coding output buffer size
    (see also ce33f3f4)
    
    The size needs to be doubled compared to previous value, otherwise
    there is an overlap between MCUs (see data_compressed out addrs in
    gpujpeg_huffman_encoder_encode_kernel_warp()). Steps to reproduce
    the problem (restart interval = 1 is required):
    
        gpujpeg -e -s 1920x1080 -r 1 -f 444-u8-p0p1p2 /dev/zero out.jpg
    
    or
    
        gpujpeg -e -s 16x16 -r 1 -f u8 /dev/zero out.jpg

--- libgpujpeg/gpujpeg_type.h
+++ libgpujpeg/gpujpeg_type.h
@@ -47,7 +47,7 @@ extern "C" {
 #define GPUJPEG_BLOCK_SIZE                      8
 #define GPUJPEG_BLOCK_SQUARED_SIZE              64
 #define GPUJPEG_MAX_COMPONENT_COUNT             4
-#define GPUJPEG_MAX_BLOCK_COMPRESSED_SIZE       (GPUJPEG_BLOCK_SIZE * GPUJPEG_BLOCK_SIZE * 4)
+#define GPUJPEG_MAX_BLOCK_COMPRESSED_SIZE       (GPUJPEG_BLOCK_SIZE * GPUJPEG_BLOCK_SIZE * 8)
 
 /** Maximum JPEG header size (MUST be divisible by 4!!!) */
 #define GPUJPEG_MAX_HEADER_SIZE                 (65536 - 100)
