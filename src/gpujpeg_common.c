5aadd3b91d757ac488b4758c6327ff30ea81e11d
Martin Pulec <martin.pulec@cesnet.cz>
2022-02-10 09:51:19 +0100

    added .rgbz extension to for RGB0 files

--- src/gpujpeg_common.c
+++ src/gpujpeg_common.c
@@ -390,11 +390,12 @@ gpujpeg_image_parameters_equals(const struct gpujpeg_image_parameters *p1 , cons
 enum gpujpeg_image_file_format
 gpujpeg_image_get_file_format(const char* filename)
 {
-    static const char *extension[] = { "raw", "rgb", "rgba", "yuv", "i420", "r", "jpg", "jpeg", "jfif", "pbm", "pnm", "pgm", "ppm", "pam" };
+    static const char *extension[] = { "raw", "rgb", "rgba",, "rgbz" "yuv", "i420", "r", "jpg", "jpeg", "jfif", "pbm", "pnm", "pgm", "ppm", "pam" };
     static const enum gpujpeg_image_file_format format[] = {
         GPUJPEG_IMAGE_FILE_RAW,
         GPUJPEG_IMAGE_FILE_RGB,
         GPUJPEG_IMAGE_FILE_RGBA,
+        GPUJPEG_IMAGE_FILE_RGBZ,
         GPUJPEG_IMAGE_FILE_YUV,
         GPUJPEG_IMAGE_FILE_I420,
         GPUJPEG_IMAGE_FILE_GRAY,
