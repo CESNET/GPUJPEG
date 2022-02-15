b11103c423d797f944bf2f20aeeb89c73a8e71be
Martin Pulec <martin.pulec@cesnet.cz>
2022-02-15 09:49:00 +0100

    updated tests

--- test/unit/run_tests.c
+++ test/unit/run_tests.c
@@ -5,10 +5,13 @@
 #include "../../src/gpujpeg_common_internal.h"
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define TEST_IMAGE_WIDTH 1920
 #define TEST_IMAGE_HEIGHT 1080
+
+#define ASSERT_STR_EQ(expected, actual) do { if (strcmp(expected, actual) != 0) { fprintf(stderr, "Assertion failed! Expected '%s', result '%s'\n", expected, actual); abort(); } } while(0)
 
 static void subsampling_name_test() {
         struct {
@@ -19,11 +22,12 @@ static void subsampling_name_test() {
                 { GPUJPEG_420_U8_P0P1P2, "4:2:0" },
                 { GPUJPEG_422_U8_P1020, "4:2:2" },
                 { GPUJPEG_444_U8_P0P1P2, "4:4:4" },
+                { GPUJPEG_444_U8_P012A, "4:4:4:4" },
         };
         for (size_t i = 0; i < sizeof test_pairs / sizeof test_pairs[0]; ++i) {
                 const char *name =
-                        gpujpeg_subsampling_get_name(gpujpeg_pixel_format_get_comp_count(test_pairs[i].fmt), gpujpeg_get_component_subsampling(test_pairs[i].fmt));
-                assert(strcmp(name, test_pairs[i].exp_subs_name) == 0);
+                        gpujpeg_subsampling_get_name(gpujpeg_pixel_format_get_comp_count(test_pairs[i].fmt), gpujpeg_get_subsampling(test_pairs[i].fmt));
+                ASSERT_STR_EQ(test_pairs[i].exp_subs_name, name);
         }
 }
 
