455132b24da7c9f62cc07036294f22b70ee902e9
Martin Pulec <martin.pulec@cesnet.cz>
2023-06-01 12:25:31 +0200

    test/regression/run_tests.sh: basic pam/pnm/y4m read/write check

--- test/regression/run_tests.sh
+++ test/regression/run_tests.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -eu
 
-GPUJPEG=${1:-$DIR/../gpujpegtool}
+DIR=$(dirname $0)
+GPUJPEG=${1:-$DIR/../../gpujpegtool}
+if [ ! -x "$GPUJPEG" ] && [ -x "$DIR/../../build/gpujpegtool" ]; then
+        GPUJPEG=$DIR/../../build/gpujpegtool
+fi
 REQUESTED_PSNR=50
 
 test_commit_b620be2() {
@@ -29,5 +33,22 @@ test_commit_b620be2() {
         rm in.rgb in.r out.rgb out.r out.jpg
 }
 
+# currently just a simple read/write tests without validating file contents
+test_pam_pnm_y4m() {
+        readonly w=256
+        readonly h=256
+        printf "YUV4MPEG2 W%d H%d F25:1 Ip A0:0 C444 XCOLORRANGE=FULL\nFRAME\n" $w $h > in.y4m
+        dd if=/dev/zero of=data.raw bs=$((w*h*3)) count=1
+        cat data.raw >> in.y4m
+        $GPUJPEG -e in.y4m out.jpg
+        $GPUJPEG -d out.jpg out.y4m
+        $GPUJPEG -d out.jpg out.pam
+        $GPUJPEG -d out.jpg out.pnm
+        $GPUJPEG -e out.pam out.jpg
+        $GPUJPEG -e out.pnm out.jpg
+        rm data.raw in.y4m out.jpg out.pam out.pnm out.y4m
+}
+
 test_commit_b620be2
+test_pam_pnm_y4m
 
