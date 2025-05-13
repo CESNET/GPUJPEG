173d1b3f76188076f3d898befc984c397dcc8eb0
Martin Pulec <martin.pulec@cesnet.cz>
2025-05-13 16:21:36 +0200

    gpujpeg_image_calculate_size: add assertion
    he values may be invalid if struct uninitialized
    
    **may** fail:
    `gpujpegtool -e 16384x1024.p_u8.tst out.jpg`
    
    tst pattern doesn't initialize the gpujpeg_image_parameters, namely
    with_padding), which occurs since the commit c35b498e (2025-05-07).

--- src/gpujpeg_common.c
+++ src/gpujpeg_common.c
@@ -1155,6 +1155,10 @@ gpujpeg_coder_deinit(struct gpujpeg_coder* coder)
 size_t
 gpujpeg_image_calculate_size(struct gpujpeg_image_parameters* param)
 {
+    assert(param->width > 0 && param->height > 0);
+    assert(param->width <= 65535 && param->height <= 65535);
+    assert(param->width_padding >= 0);
+    assert(param->width_padding < 100000); // safety check - 100k chosen arbitrarily
     int bpp = gpujpeg_pixel_format_get_unit_size(param->pixel_format);
     if (bpp != 0) {
         return ((size_t) param->width + param->width_padding) * param->height * bpp;
